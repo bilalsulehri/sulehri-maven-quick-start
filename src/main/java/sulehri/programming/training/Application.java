695d0ea71c06ca3f4ecb98fe56032f7bd85f10fd
bilalsulehri <bilalsulehri@gmail.com>
2020-04-19 12:22:43 +0500

    Adding First Java File

---
+++ src/main/java/sulehri/programming/training/Application.java
@@ -0,0 +1,15 @@
+package sulehri.programming.training;
+
+public class Application {
+	
+	public Application() {
+		System.out.println("Inside application");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Starting application");
+		
+		Application app=new Application();
+	}
+
+}
