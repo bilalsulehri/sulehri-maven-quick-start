41f11c6d06d769bb242bc80634ca65fd2dd0fa30
bilalsulehri <bilalsulehri@gmail.com>
2020-04-19 13:37:37 +0500

    Changing compiler plugin to 1.8

--- src/main/java/sulehri/programming/training/Application.java
+++ src/main/java/sulehri/programming/training/Application.java
@@ -1,15 +1,26 @@
 package sulehri.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 	
 	public Application() {
 		System.out.println("Inside application");
 	}
 	
+	public void greet() {
+		List<String> greetings=new ArrayList<String>();
+		greetings.add("Hello World");
+		for(String greeting:greetings) {
+			System.out.println(greeting);
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Starting application");
 		
 		Application app=new Application();
+		app.greet();
 	}
 
 }
